77f2bca01890361ead9b424b13961781ba4c2570
Luiz Gustavo da Silva Vasconcellos <iamtheluiz.dev@gmail.com>
2018-07-11 21:03:03 -0300

    Adicionada licença ao arquivo 'js/2048_game.js'

--- js/2048_game.js
+++ js/2048_game.js
@@ -1,3 +1,20 @@
+// Copyright (C) 2018  Luiz Gustavo da Silva Vasconcellos
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PART ICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+////////////////////////////////////////////////////////////////////////////
+
+
 //Variáveis Globais do Jogo
 var y = 4;  //Define o número de quadrados na altura 
 var x = 4;  //Define o número de quadrados na largura (caso esse número seja alterado, é necessário modificar no arquivo 'css/style.css' o width da class '.quadrado', onde se encontra um calc com divisão pelo número de quadrados de largura (100%/4), só é necessária a modificação do denominador)
